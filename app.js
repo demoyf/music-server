8cec902ca84f528c907d3817315d26d62c3acf81
xyf <425746853@qq.com>
2018-01-23 23:22:23 +0800

    redis hot artist

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var billboard = require('./routes/billboard');
+var artist = require('./routes/artist');
 var app = express();
 
 // view engine setup
@@ -25,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/billboard',billboard);
-
+app.use('/artist',artist);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/artist.js
+++ routes/artist.js
@@ -6,6 +6,7 @@ const QUERY_UTIL = require('./../lib/net/get_data');
 const _query_db = require('./../lib/db/query_db');
 const _redis = require('./../lib/redis/redis_util');
 let http_redis = require('./../lib/redis/http_redis');
+let _page_and_param = require('./../lib/redis/page_and_param');
 router.get('/', function(req, res, next) {
     let id = req.query.tinguid;
     if (!id) {
@@ -27,18 +28,23 @@ router.get('/', function(req, res, next) {
         }
     });
 });
-router.get('/', function(req, res, next) {
+router.get('/hot_artist', function(req, res, next) {
 	let page = req.query.page||0;
 	res.type("text/javascript");
-    _redis.get_hot_artist(page).then((redis_result => {
+    let param = _page_and_param.hot_artist;
+    _redis.get_form_list(param.key,page).then((redis_result => {
         if (redis_result) {
+            console.log("hot artist in redis");
         	res.end(redis_result);
+            return;
         } else {
+            console.log("hot artist in network");
             QUERY_UTIL.Net_getHotArtist().then((hot_result) => {
             	res.end(hot_result);
             	_redis.save
             });
             http_redis.request_hot_artist();
+            return;
         }
     }));
 
