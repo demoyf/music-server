6387a6c138d0029cb35f0aaacfef6c29d4f52360
xyf <425746853@qq.com>
2018-01-26 00:20:40 +0800

    new song

--- routes/artist.js
+++ routes/artist.js
@@ -24,11 +24,25 @@ router.get('/hot_artist', function(req, res, next) {
     let param = _page_and_param.hot_artist;
     // http_redis.request_hot_artist();
     // res.end("123");
-    _redis.get_form_list(param.key, 1).then((redis_result => {
-        if (redis_result) {
-            console.log("hot artist in redis");
-            res.end(redis_result);
-            return;
+    let artist_obj = {};
+    _redis.getStringByKey(param.redis_name).then((name_result) => {
+        if (name_result) {
+            artist_obj.name = JSON.parse(name_result);
+            _redis.get_form_list(param.key, 1).then((redis_result => {
+                if (redis_result) {
+                    console.log("hot artist in redis");
+                    artist_obj.artist = JSON.parse(redis_result);
+                    res.end(JSON.stringify(artist_obj));
+                    return;
+                } else {
+                    console.log("hot artist in network");
+                    QUERY_UTIL.Net_getHotArtist().then((hot_result) => {
+                        res.end(hot_result);
+                    });
+                    http_redis.request_hot_artist();
+                    return;
+                }
+            }));
         } else {
             console.log("hot artist in network");
             QUERY_UTIL.Net_getHotArtist().then((hot_result) => {
@@ -37,7 +51,11 @@ router.get('/hot_artist', function(req, res, next) {
             http_redis.request_hot_artist();
             return;
         }
-    }));
+    });
 });
 
+router.get('/get_artist/:ting',function(req,res,next){
+    console.log(req.params.ting);
+    res.end("123");
+});
 module.exports = router;
