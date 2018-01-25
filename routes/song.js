ef846b57ee65c90496637dbaa0779cb4f588ce73
xyf <425746853@qq.com>
2018-01-26 01:14:41 +0800

    get song by id

--- routes/song.js
+++ routes/song.js
@@ -5,6 +5,8 @@ const QUERY_UTIL = require('./../lib/net/get_data'); //网络请求
 const _redis = require('./../lib/redis/redis_util'); //获取redis
 let http_redis = require('./../lib/redis/http_redis'); // 请求完整数据并且保存到数据库
 let _page_and_param = require('./../lib/redis/page_and_param'); // 获取配置参数
+let _db_key = require('./../lib/db/db_key');
+let _my_db = require('./../lib/db/mydb');
 router.get('/new_song', function(req, res, next) {
     let param = _page_and_param.new_song;
     let page = req.query.page || 0;
@@ -38,5 +40,28 @@ router.get('/new_song', function(req, res, next) {
         }
     });
 });
-
+router.get('/get_song/:song_id', function(req, res, next) {
+    let song_id = req.params.song_id;
+    let key = _db_key.song;
+    res.type("text/json");
+    _my_db.queryData(key.collection, { _id: song_id }).then((data) => {
+        if (data) {
+            console.log("song in db")
+            res.end(JSON.stringify(data));
+        } else {
+            QUERY_UTIL.net_get_song(song_id).then((song_data) => {
+                if (song_data) {
+                    console.log("song in net");
+                    res.end(JSON.stringify(song_data));
+                    let temp = JSON.parse(song_data);
+                    temp._id = temp.songinfo.song_id;
+                    _my_db.insertData(key.collection, temp);
+                    return;
+                } else {
+                    res.end("not found");
+                }
+            });
+        }
+    });
+});
 module.exports = router;
